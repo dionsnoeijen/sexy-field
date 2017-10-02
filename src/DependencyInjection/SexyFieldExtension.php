9db5e6d901f748ade92faffbb5e7aeddbf994134
Dion Snoeijen <hallo@dionsnoeijen.nl>
2017-10-02 12:57:00 +0200

    Renamed extension to be more sexy

--- src/DependencyInjection/SexyFieldExtension.php
+++ src/DependencyInjection/SexyFieldExtension.php
@@ -18,7 +18,7 @@ use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Extension\Extension;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 
-class SectionFieldExtension extends Extension
+class SexyFieldExtension extends Extension
 {
     public function load(array $configs, ContainerBuilder $container)
     {
