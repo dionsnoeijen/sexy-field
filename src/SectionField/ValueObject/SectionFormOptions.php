ba4711b01a3524254c3d2087edbe4af35f0f4c9a
Dion Snoeijen <hallo@dionsnoeijen.nl>
2017-10-07 11:17:00 +0200

    Fixed slug namespace

--- src/SectionField/ValueObject/SectionFormOptions.php
+++ src/SectionField/ValueObject/SectionFormOptions.php
@@ -14,7 +14,6 @@ declare (strict_types = 1);
 namespace Tardigrades\SectionField\ValueObject;
 
 use Assert\Assertion;
-use Tardigrades\FieldType\Slug\ValueObject\Slug;
 
 final class SectionFormOptions
 {
