5a1363406562b8a6e0c3b6207a22361157b024fe
Paulo <p.moreno@ltp.nl>
2017-10-20 16:08:06 +0200

    adjust Created datetype, add tests for Created and CreatedField VO's

---
+++ test/unit/SectionField/ValueObject/CreatedFieldTest.php
@@ -0,0 +1,34 @@
+<?php
+declare (strict_types=1);
+
+namespace Tardigrades\SectionField\ValueObject;
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @coversDefaultClass Tardigrades\SectionField\ValueObject\CreatedField
+ * @covers ::<private>
+ * @covers ::__construct
+ */
+class CreatedFieldTest extends TestCase
+{
+    /**
+     * @test
+     * @covers ::fromString
+     */
+    public function it_should_create_from_String()
+    {
+        $this->assertInstanceOf(CreatedField::class, CreatedField::fromString('I am a created field!'));
+    }
+
+    /**
+     * @test
+     * @covers ::fromString
+     */
+    public function it_should_throw_exception_if_empty()
+    {
+        $this->expectException('InvalidArgumentException');
+        $this->expectExceptionMessage('Value is not specified');
+        CreatedField::fromString('');
+    }
+}
