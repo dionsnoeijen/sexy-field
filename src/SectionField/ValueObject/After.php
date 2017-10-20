b86e84e303846b3f0a1d1cf0ccc4f6bb8bcfd843
Paulo <p.moreno@ltp.nl>
2017-10-20 15:03:53 +0200

    fix dates in After/Before objects, add tests

--- src/SectionField/ValueObject/After.php
+++ src/SectionField/ValueObject/After.php
@@ -27,12 +27,12 @@ final class After
 
     public function __toString()
     {
-        return $this->dateTime->format('D-m-y h:i');
+        return $this->dateTime->format('Y-m-d\TH:i');
     }
 
     public static function fromString(string $dateTime): self
     {
-        $format = 'D-m-y h:i';
+        $format = 'Y-m-d\TH:i';
         Assertion::date($dateTime, $format);
         $dateTime = \DateTimeImmutable::createFromFormat($format, $dateTime);
 
---
+++ test/unit/SectionField/ValueObject/BeforeTest.php
@@ -0,0 +1,47 @@
+<?php
+declare (strict_types=1);
+
+namespace Tardigrades\SectionField\ValueObject;
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @coversDefaultClass Tardigrades\SectionField\ValueObject\Before
+ * @covers ::<private>
+ * @covers ::__construct
+ */
+class BeforeTest extends TestCase
+{
+    /**
+     * @test
+     * @covers ::fromString
+     */
+    public function it_should_create_from_String()
+    {
+        $datetime = '2000-12-12T12:01';
+        $before = Before::fromString($datetime);
+        $this->assertInstanceOf(Before::class, $before);
+    }
+
+    /**
+     * @test
+     * @covers ::fromDateTime
+     */
+    public function it_should_create_from_DateTime()
+    {
+        $datetime = new \DateTime();
+        $before = Before::fromDateTime($datetime);
+        $this->assertInstanceOf(Before::class, $before);
+    }
+
+    /**
+     * @test
+     * @covers ::fromString
+     */
+    public function it_should_throw_exception_if_string_not_in_good_format()
+    {
+        $this->expectException('InvalidArgumentException');
+        $this->expectExceptionMessage('Date "two thousand-Dec-12T12:01" is invalid or does not match format "Y-m-d\TH:i"');
+        Before::fromString('two thousand-Dec-12T12:01');
+    }
+}
