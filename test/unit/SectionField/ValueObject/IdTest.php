07917932d9840f8c8bdca2d4c787e501e173f0a5
Paulo <p.moreno@ltp.nl>
2017-10-20 17:40:48 +0200

    adjust Id VO, add respective unit tests

---
+++ test/unit/SectionField/ValueObject/IdTest.php
@@ -0,0 +1,26 @@
+<?php
+declare (strict_types=1);
+
+namespace Tardigrades\SectionField\ValueObject;
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @coversDefaultClass Tardigrades\SectionField\ValueObject\Id
+ * @covers ::<private>
+ * @covers ::__construct
+ */
+class IdTest extends TestCase
+{
+    /**
+     * @test
+     * @covers ::fromInt
+     */
+    public function it_should_create_from_int()
+    {
+        $thing = Id::fromInt(1234567890);
+        $this->assertInstanceOf(Id::class, $thing);
+        $this->assertSame($thing->toInt(),1234567890);
+        $this->assertSame($thing->getId(),1234567890);
+    }
+}
