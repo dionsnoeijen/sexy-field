56bfbb91d6c71d8002df13037b547e380eb3422a
jcdejong <jcdejong@allict.nl>
2017-10-30 23:24:22 +0100

    added missing tests + fixed bug where a getId() would be called on non existing languages

--- test/unit/Command/DeleteLanguageCommandTest.php
+++ test/unit/Command/DeleteLanguageCommandTest.php
@@ -11,6 +11,7 @@ use Symfony\Component\Console\Tester\CommandTester;
 use Tardigrades\Entity\Language;
 use Tardigrades\Entity\Application as ApplicationEntity;
 use Tardigrades\SectionField\Service\DoctrineLanguageManager;
+use Tardigrades\SectionField\Service\LanguageNotFoundException;
 
 /**
  * @coversDefaultClass Tardigrades\Command\DeleteLanguageCommand
@@ -107,6 +108,104 @@ final class DeleteLanguageCommandTest extends TestCase
 
         $this->assertRegExp(
             '/Removed!/',
+            $commandTester->getDisplay()
+        );
+    }
+
+    /**
+     * @test
+     * @covers ::configure
+     * @covers ::execute
+     */
+    public function it_should_notify_user_when_all_languages_are_deleted()
+    {
+        $command = $this->application->find('sf:delete-language');
+        $commandTester = new CommandTester($command);
+
+        $fields = $this->givenAnArrayOfLanguages();
+        unset($fields[1]);
+
+        $this->languageManager
+            ->shouldReceive('readAll')
+            ->once()
+            ->andReturn($fields);
+
+        $this->languageManager
+            ->shouldReceive('read')
+            ->once()
+            ->andReturn($fields[0]);
+
+        $this->languageManager
+            ->shouldReceive('delete')
+            ->once();
+
+        $this->languageManager
+            ->shouldReceive('readAll')
+            ->once()
+            ->andThrow(LanguageNotFoundException::class);
+
+        $commandTester->setInputs([1, 'y']);
+        $commandTester->execute([
+            'command' => $command->getName()
+        ]);
+
+        $this->assertRegExp(
+            '/No languages left/',
+            $commandTester->getDisplay()
+        );
+    }
+
+
+    /**
+     * @test
+     * @covers ::configure
+     * @covers ::execute
+     */
+    public function it_should_not_delete_language_when_user_does_not_confirm()
+    {
+        $command = $this->application->find('sf:delete-language');
+        $commandTester = new CommandTester($command);
+        $fields = $this->givenAnArrayOfLanguages();
+        $this->languageManager
+            ->shouldReceive('readAll')
+            ->once()
+            ->andReturn($fields);
+        $this->languageManager
+            ->shouldReceive('read')
+            ->once()
+            ->andReturn($fields[0]);
+        $this->languageManager
+            ->shouldNotReceive('delete');
+        $commandTester->setInputs([1, 'n']);
+        $commandTester->execute(['command' => $command->getName()]);
+        $this->assertRegExp(
+            '/Cancelled/',
+            $commandTester->getDisplay()
+        );
+    }
+
+
+    /**
+     * @test
+     * @covers ::configure
+     * @covers ::execute
+     */
+    public function it_should_not_try_to_delete_non_existing_languages()
+    {
+        $command = $this->application->find('sf:delete-language');
+        $commandTester = new CommandTester($command);
+        $fields = $this->givenAnArrayOfLanguages();
+        $this->languageManager
+            ->shouldReceive('readAll')
+            ->once()
+            ->andReturn($fields);
+        $this->languageManager
+            ->shouldReceive('read')
+            ->andThrow(LanguageNotFoundException::class);
+        $commandTester->setInputs([9, 'y']);
+        $commandTester->execute(['command' => $command->getName()]);
+        $this->assertRegExp(
+            '/Language not found/',
             $commandTester->getDisplay()
         );
     }
