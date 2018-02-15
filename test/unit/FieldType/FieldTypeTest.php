a70be280eb35e832e716001143416e3b3d112639
Dion Snoeijen <hallo@dionsnoeijen.nl>
2018-02-15 15:11:25 +0100

    Added a bit flexibility to it_should_return_its_directory to make it succeed locally and in travis-ci

--- test/unit/FieldType/FieldTypeTest.php
+++ test/unit/FieldType/FieldTypeTest.php
@@ -75,7 +75,7 @@ final class FieldTypeTest extends TestCase
     public function it_should_return_its_directory()
     {
         $fieldType = $this->givenAFieldType();
-        $this->assertStringEndsWith('phpunit-mock-objects', $fieldType->directory());
+        $this->assertRegExp('/phpunit-mock-objects/', $fieldType->directory());
     }
 
     private function givenAFieldType()
